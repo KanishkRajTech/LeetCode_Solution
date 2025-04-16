348ddb43eaa5fca4c56a805425fa53baa797ea46
KANISHK RAJ <kanishkraj601@gmail.com>
2025-04-16 21:25:20 +0530

    2537 Count the Number of Good Subarrays

---
+++ Array/2537_Count_the_Number_of_Good_Subarrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        unordered_map<int, long long> freq;
+        long long ans = 0, pairs = 0;
+        int left = 0;
+        int n = nums.size();
+
+        for (int right = 0; right < n; ++right) {
+            pairs += freq[nums[right]];
+            freq[nums[right]]++;
+
+            while (pairs >= k) {
+                ans += n - right;
+                freq[nums[left]]--;
+                pairs -= freq[nums[left]];
+                left++;
+            }
+        }
+
+        return ans;
+    }
+};
