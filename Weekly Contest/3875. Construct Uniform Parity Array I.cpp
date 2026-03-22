385d162c6e3e13b3218295ce3ef20a66b34000e8
KANISHK RAJ <kanishkraj601@gmail.com>
2026-03-22 11:48:09 +0000

    3875. Construct Uniform Parity Array I

---
+++ Weekly Contest/3875. Construct Uniform Parity Array I.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // int n = nums1.size();
+        // vector<int> nums2(n);
+        // int odd =-1;
+        // for(int x :nums1){
+        //     if(x %2 !=0){
+        //         odd = x;
+        //         break;
+        //     }
+        // }
+
+        // for(int i =0;i<n;i++){
+        //     if(nums1[i] %2 ==0){
+        //         nums2[i] =nums1[i]-odd;
+        //     }
+        //     else{
+        //         nums2[i]= nums1[i];
+        //     }
+        // }
+
+        // bool allOdd = true, allEven = true;
+        // for(int x : nums2){
+        //     if(x % 2 == 0) allOdd = false;
+        //     else allEven = false;
+        // }
+        // return allOdd || allEven;
+
+        return true;
+    }
+};
