893ff27ee6ed660ea62860863e44ef4194ac757a
KANISHK RAJ <kanishkraj601@gmail.com>
2026-04-06 16:53:16 +0000

    503. Next Greater Element II

---
+++ Stack/503. Next Greater Element II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int>st;
+        int n = nums.size() ;
+        vector<int>ans(n,-1);
+        for(int i = 2*n-1; i >= 0; i--){
+            // int num = nums[i];
+            // if(((i +1)% n)== 0)
+            //     i = (i +1)% n;
+            while( !st.empty() && st.top() <= nums[i%n])
+                st.pop();
+            if(st.empty()){
+                ans[i%n] = -1;
+            }else{
+                ans[i%n] = st.top();
+            }
+            st.push(nums[i%n]);
+        }
+        return ans;
+    }
+};
